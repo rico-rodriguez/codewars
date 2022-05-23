d10930e850765e592f9d1d41aa1e3bb79d012473
Rico Rodriguez <97664519+The-Rico@users.noreply.github.com>
2022-05-23 16:12:01 -0700

    Complete the function to return his total number of goals
    For example:
    
    5, 10, 2  -->  17

---
+++ messiGoals.js
@@ -0,0 +1,17 @@
+function goals (a, b, c) {
+  // code goes here
+  return a + b + c
+}
+
+/*Messi is a soccer player with goals in three leagues:
+
+LaLiga
+Copa del Rey
+Champions
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17*/
