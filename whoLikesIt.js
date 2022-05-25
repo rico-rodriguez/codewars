1eeb3995e5fd23ce606b4e7ae21b10bcc1cd2329
Rico Rodriguez <97664519+The-Rico@users.noreply.github.com>
2022-05-25 14:29:27 -0700

    Return a string stating how many people like it

---
+++ whoLikesIt.js
@@ -0,0 +1,7 @@
+function likes(names) {
+return names.length === 0 ? 'no one likes this' : names.length === 1 ? names[0] + ' likes this' :
+names.length === 2 ? names[0] + ' and ' + names[1] + ' like this' :
+names.length === 3 ? names[0] + ', ' + names[1] + ' and '+ names[2] + ' like this' :
+names.length >= 4 ? names[0] + ', ' + names[1] + ' and ' + (names.length - 2) + ' others like this' : null
+
+}
